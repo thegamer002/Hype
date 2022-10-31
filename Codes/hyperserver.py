fb0669bb609a928e2dedc2dc4f193b5e016cf42b
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-10-30 21:55:11 -0300

    command work  now
    before commands not work, now his its working

--- Codes/hyperserver.py
+++ Codes/hyperserver.py
@@ -1,6 +1,3 @@
-# Tell the app which of its api versions we are written for. The app's
-# meta-scanner will skip this file if this number doesn't match theirs.
-# To learn more, see https://ballistica.net/wiki/meta-tag-system
 # ba_meta require api 7
 
 import ba
@@ -12,7 +9,7 @@ class _cmd():
     
     teams = ['brazil', 'latam', 'egypt', 'algeria', 'turkey', 'russia', 'italy']
     def _home():
-        messages = get_chat_messages()
+        messages = _ba.get_chat_messages()
         if len(messages)>1:
             lmsg = messages[len(messages)-1]
             
@@ -25,7 +22,7 @@ class _cmd():
             
             
     def commands():
-        messages = get_chat_messages()
+        messages = _ba.get_chat_messages()
         if len(messages)>1:
             lmsg = messages[len(messages)-1]
             
@@ -36,17 +33,15 @@ class _cmd():
                 msg("Use /teams brazil,argentina to change teams and /timer to 10 minutes on screen")
             
         
-        
-# ba.timer(0.05, _update, repeat=True)      
+
 def readCmd():
     msg(f'God Commands')
-    ba.timer(0.5, _cmd._home, True)
-# Tell the app about our Plugin.
+    ba.timer(5.0, _cmd._home, True)
 
-
-# ba_meta export plugin
+    # ba_meta export plugin
 
 class TimerCmd(ba.Plugin):
     """My awesome plugin."""
     ba.internal.set_party_icon_always_visible(True)
     readCmd()
+   
