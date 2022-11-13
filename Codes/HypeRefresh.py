495ee4470d9e7a7baf2f90928499a82530485464
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-12 21:49:03 -0300

    update HypeRefresh.py

---
+++ Codes/HypeRefresh.py
@@ -0,0 +1,48 @@
+# ba_meta require api 7
+
+from _ba import chatmessage as msg
+import ba
+import _ba
+import os 
+
+
+class _use():
+    
+    def _home():
+        messages = _ba.get_chat_messages()
+        if len(messages)>1:
+            lmsg = messages[len(messages)-1]
+            
+            c = lmsg.split(' ')[1]
+            if c.startswith(';'):
+                return _cmd.commands()
+            else:
+                pass
+            
+            
+            
+            
+    def refresh():
+        
+        messages = _ba.get_chat_messages()
+        if len(messages)>1:
+            lmsg = messages[len(messages)-1]
+            
+            c = lmsg.split(' ')[1]
+            a = lmsg.split(' ')[2:]
+            
+            if c in ['/ref', '/refresh]:
+                msg("Server Refreshing Mods!, Enter In one minute.")
+                
+                
+                
+                
+def readCmd():
+    ba.timer(0.5, _cmd._home, True)
+
+# ba_meta export plugin
+
+class TimerCmd(ba.Plugin):
+    """My awesome plugin."""
+    ba.internal.set_party_icon_always_visible(True)
+    readCmd()   
