1cbf575983cfd2ef21db236ad1421399bbc529ce
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-14 20:25:21 -0300

    update HypeRefresh.py

--- Codes/HypeRefresh.py
+++ Codes/HypeRefresh.py
@@ -61,7 +61,7 @@ class _cmd():
     
 def readCmd():
     ba.timer(0.5, _cmd._home, True)
-    ba.timer(5.0, ba.internal.chatmessage("Server is in Testing!! ")), True)
+    ba.timer(5.0, ba.internal.chatmessage("Server is in Testing!! "), True)
 # ba_meta export plugin
 
 class TimerCmd(ba.Plugin):
