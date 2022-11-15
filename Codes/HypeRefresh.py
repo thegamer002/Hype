77df001f67e4dd80d8587c37545cf68fe497a3a5
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-14 23:03:02 -0300

    update HypeRefresh.py

--- Codes/HypeRefresh.py
+++ Codes/HypeRefresh.py
@@ -32,9 +32,9 @@ class _cmd():
             if player.inputdevice.get_v1_account_id() == ClientID:
                 admins = player.get_v1_account_id()
         if admins in htgs.Admin:
-            return True
+            return _cmd.commands
         else: 
-            return False
+            return _cmd.PermissionCheck
             
             
     def commands():
@@ -45,33 +45,33 @@ class _cmd():
             
             c = lmsg.split(' ')[1]
             a = lmsg.split(' ')[2:]
+            if PermissionCheck(ClientID) == True:
+                if c == '/pwd':
+                    os.system("pwd > cmd.txt")
+                    f = open("cmd.txt", "r")
+                    msg(f" Server Path Is : {f.read()}")
                 
-            if c == '/pwd':
-                os.system("pwd > cmd.txt")
-                f = open("cmd.txt", "r")
-                msg(f" Server Path Is : {f.read()}")
-            
-            
-            if c in ['/ref', '/refresh']:
-                msg("Server Refreshing Mods!, Enter In one minute.")
-                os.system("sudo rm -rf HypeServerStaff/ && gh repo clone thegamer002/HypeServerStaff && sudo cp HypeServerStaff/Codes/* ba_root/mods/ && sudo rm -rf HypeServerStaff/")
-                msg("_____________Upload Complete______________")
                 
-            if c == '/mods':
-                os.system("sudo ls ba_root/mods/ > cmd.txt")
-                msg("Archives or Mods")
-                with open("cmd.txt", "r") as cmd:
-                    command = cmd.readlines()
-                for lin in command: 
-                    msg(lin)
-            
-            if c in ['/l', '/list']:
-                session = _ba.get_foreground_host_session().sessionplayers
-                msg("|--------•NAME•--------|----------•ID•----------|")
-                for player in session:
-                    msg(f"|{player.getname(True, True)} ---> {player.id} or {player.inputdevice.client_id}")
-                msg("|-----------------------------------------------|")
+                if c in ['/ref', '/refresh']:
+                    msg("Server Refreshing Mods!, Enter In one minute.")
+                    os.system("sudo rm -rf HypeServerStaff/ && gh repo clone thegamer002/HypeServerStaff && sudo cp HypeServerStaff/Codes/* ba_root/mods/ && sudo rm -rf HypeServerStaff/")
+                    msg("_____________Upload Complete______________")
+                    
+                if c == '/mods':
+                    os.system("sudo ls ba_root/mods/ > cmd.txt")
+                    msg("Archives or Mods")
+                    with open("cmd.txt", "r") as cmd:
+                        command = cmd.readlines()
+                    for lin in command: 
+                        msg(lin)
                 
+                if c in ['/l', '/list']:
+                    session = _ba.get_foreground_host_session().sessionplayers
+                    msg("|--------•NAME•--------|----------•ID•----------|")
+                    for player in session:
+                        msg(f"|{player.getname(True, True)} ---> {player.id} or {player.inputdevice.client_id}")
+                    msg("|-----------------------------------------------|")
+            else:
                 
                 
     
