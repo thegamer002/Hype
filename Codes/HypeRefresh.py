ecf88c38f32d159f3c7599b9af7b56ffee75cb58
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-21 22:13:33 -0300

    update HypeRefresh.py

--- Codes/HypeRefresh.py
+++ Codes/HypeRefresh.py
@@ -1,50 +1,40 @@
 # ba_meta require api 7
 
+#odules
 from ba.internal import get_chat_messages as gmsg, chatmessage as msg
-import ba.internal as bs
 import ba
 import _ba
 import os
-import HypeTagsHashes as htgs
 
 
-
-
+#Main Code
 class _cmd():
-    def testingserver():
-        msg("Server is in testing")
-        
+
+
     def _home():
+        
         messages = gmsg()
         if len(messages)>1:
             lmsg = messages[len(messages)-1]
             
             c = lmsg.split(' ')[1]
-            if c.startswith('/' or ';'):
+            
+            if c.startswith('/'):
                 return _cmd.commands()
             else:
                 pass
-    
-    def PermissionCheck(self, ClientID):
-        session = _ba.get_foreground_host_session().sessionplayers
-        admins = []
-        for player in session:
-            if player.inputdevice.get_v1_account_id() == ClientID:
-                admins = player.get_v1_account_id()
-        if admins in htgs.Admin:
-            return True
-        else: 
-            return _cmd.PermissionCheck
-            
-            
-    def commands(self, ClientID):
+
+
+    def commands():
+        
+        
         messages = gmsg()
         if len(messages)>1:
             lmsg = messages[len(messages)-1]
-        
+
             c = lmsg.split(' ')[1]
             a = lmsg.split(' ')[2:]
-        
+            
             if c in ['/help', '/']:
                 msg("___________________________________")
                 msg("|/pwd - To show current Path       |")
@@ -52,8 +42,8 @@ class _cmd():
                 msg("|/mods - To see mods in directory  |")
                 msg("|/list, /l - To see players id     |")
                 msg("|/help - To see commands           |")
-                msg("|___________________________________|")
-            
+                msg("|__________________________________|")
+                
             elif c == '/pwd':
                 os.system("pwd > cmd.txt")
                 f = open("cmd.txt", "r")
@@ -65,34 +55,51 @@ class _cmd():
                 os.system("sudo rm -rf HypeServerStaff/ && gh repo clone thegamer002/HypeServerStaff && sudo cp HypeServerStaff/Codes/* ba_root/mods/ && sudo rm -rf HypeServerStaff/")
                 msg("_____________Upload Complete______________")
                 
+                
             elif c == '/mods':
-                os.system("sudo ls ba_root/mods/ > cmd.txt")
-                msg("Archives or Mods")
+                mods = os.system("sudo ls ba_root/mods/ > cmd.txt ")
+                
+                if mods == '0':
+                    pass
+                
+                else:
+                    os.system("ls  > cmd.txt")
+                
+                msg("_______________________")
+                
                 with open("cmd.txt", "r") as cmd:
                     command = cmd.readlines()
                 for lin in command: 
                     msg(lin)
-            
+                
+                msg("===Archives or Mods===")
+                
+                
             elif c in ['/l', '/list']:
                 session = _ba.get_foreground_host_session().sessionplayers
-                msg("_________________________________________________")
-                msg("|         •NAME•        |          •IDs•        |")
-                msg("|_______________________|_______________________|")
-                for player in session:
-                    msg(f"|{player.getname(True, True)} ---> {player.id} or {player.inputdevice.client_id}")
-                msg("|_______________________________________________|")
-         
-        
                 
-                
-    
+                if session == []:
+                    msg("Players Not Found")
+                else:
+                    msg("== Name | IDs ==")
+                    for player in session:
+                        msg(f"{player.getname(True, True)[0:9]} ----> {player.id}")
+                    msg("")
+                    msg("== For kick only ==")
+                    
+                    for player in session:
+                        msg(f"{player.getname(True, True)[0:9]} ----> {player.inputdevice.client_id}")
+                    msg("===============")
+
+
+
 def readCmd():
-    ba.timer(0.5, _cmd._home, True)
+    ba.timer(0.2, _cmd._home, True)
     #ba.timer(5.0, _cmd.testingserver, False)
     
 # ba_meta export plugin
 
-class TimerCmd(ba.Plugin):
+class cmds(ba.Plugin):
     """My awesome plugin."""
     ba.internal.set_party_icon_always_visible(True)
     readCmd()   
