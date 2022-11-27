fabaef0c18df2cbc767de70d0c063251edfec771
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-27 20:34:19 -0300

    update HypeRefresh.py

--- Codes/HypeRefresh.py
+++ Codes/HypeRefresh.py
@@ -38,7 +38,7 @@ class _cmd():
             a = lchatmsg.split(' ')[2:]
             playername = lchatmsg.split(':')[0]
             
-            if playername in ['\ue035Coutinho', '\ue064VinesCraft']:
+            if playername in ['\ue035Coutinho', '\ue063VinesCraft']:
                 
                 if c == '/pwd':
                     chatmsg(lchatmsg)
