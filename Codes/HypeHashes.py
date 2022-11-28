b38f1ca8b359b1e677b68e60df3c87094894dc98
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-28 16:14:37 -0300

    update HypeHashes.py

--- Codes/HypeHashes.py
+++ Codes/HypeHashes.py
@@ -14,5 +14,6 @@ Admin = ['pb-IF5XVVVTFA==', #ZODIC
         'pb-IF4jUkE8Nw==', #snobbish
         'pb-IF4yV0VfJw==', #ice
         'pb-IF5WU2MtLA==', #snobbish2
-        'pb-IF4WPm1a' #Coutinho FOR TESTS
+        'pb-IF4WPm1a', #Coutinho FOR TESTS
+        'pb-IF4xV1g5Cg==' #Stratex
  ]
