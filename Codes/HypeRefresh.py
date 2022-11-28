596ab44aed82e68bcf23e5f4fd36d0962852b9a3
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2022-11-27 21:57:57 -0300

    update HypeRefresh.py

--- Codes/HypeRefresh.py
+++ Codes/HypeRefresh.py
@@ -102,7 +102,7 @@ class _cmd():
                     try:
                         ba.screenmessage("Game ending", (0,1,0))
                         _ba.get_foreground_host_activity().end_game()
-                    except: chatmsg("End game Error")
+                    except: ba.screenmessage("Game ending", (0,1,0))
                         
                     
                 else:
