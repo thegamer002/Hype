dc22a7277364ecf59b44c8ff53eb58c097023332
CoutinhoBombsquad <83516882+thegamer002@users.noreply.github.com>
2023-01-16 12:08:35 -0300

    update Hype.py

---
+++ Codes/Hype.py
@@ -0,0 +1,11 @@
+class html:
+    
+    
+    def __init__(self):
+        self.stats = "!!RANK OF STATS IS HERE!!"
+    
+    def show(self):
+        print("""<div class='hype'>
+        
+        
+        </div>""")
